e3cfac52201ff80b21e8f5366094639d4a83984b
wasidennis <wasidennis@gmail.com>
2019-10-16 14:26:29 -0700

    update codes for LS-GAN

--- pytorch_0.4/evaluate_cityscapes.py
+++ pytorch_0.4/evaluate_cityscapes.py
@@ -99,6 +99,11 @@ def main():
         saved_state_dict = model_zoo.load_url(args.restore_from)
     else:
         saved_state_dict = torch.load(args.restore_from)
+    ### for running different versions of pytorch
+    model_dict = model.state_dict()
+    saved_state_dict = {k: v for k, v in saved_state_dict.items() if k in model_dict}
+    model_dict.update(saved_state_dict)
+    ###
     model.load_state_dict(saved_state_dict)
 
     device = torch.device("cuda" if not args.cpu else "cpu")
